161f731b07d453eec96b5f2be37b487b8af47ecc
Zen Farhat <zenfarhat@Zens-MacBook-Air.local>
2024-08-16 17:39:20 +1000

    update

---
+++ drizzle.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "drizzle-kit"
+import { config } from "dotenv"
+
+config({ path: ".env.local" })
+
+export default defineConfig({
+  schema: "./src/db/schema.ts",
+  out: "./migrations",
+  dialect: "postgresql",
+
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+
+  verbose: true,
+  strict: true,
+})
